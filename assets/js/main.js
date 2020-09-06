c957f63fa641490676ec7d33fd67813357185081
Max Plumer <68719068+mplumer@users.noreply.github.com>
2020-09-05 21:53:28 -0400

    add save feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,30 @@
 // DISPLAY CURRENT DATE IN HEADER
+
 $("#currentDay").text(moment().format('dddd, MMMM DD'));
+
+// ONE BUTTON SAVES ALL INPUTS
+
+$(document).ready(function () {
+
+    var saveBtn = document.querySelector(".saveBtn");
+
+    for (var i = 0; i < 9; i++) {
+        $("#slot" + i).val(localStorage.getItem("slot" + i));
+    }
+
+    function saveEntry() {
+        for (var i = 0; i < 9; i++) {
+            localStorage.setItem("slot" + i, $("#slot" + i).val());
+        }
+    };
+
+    saveBtn.addEventListener("click", saveEntry);
+})
+
+
+// SAVE USER INPUT TO LOCAL STORAGE
+
+$(".saveBtn").click(function (event) {
+    localStorage.setItem($(this).attr("id"), $(this).siblings(".user-input").text());
+
+}) 
